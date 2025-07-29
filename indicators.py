26b010c43d17966d2b6261ee548683c2824a8a5f
vaibhav709a <aspandey02e@gmail.com>
2025-07-29 19:13:49 +0530

    Delete indicators.py

--- indicators.py
+++
@@ -1,8 +0,0 @@
-### FILE: indicators/indicators.py
-import ta
-
-def add_indicators(df):
-    df["rsi"] = ta.momentum.RSIIndicator(df["close"]).rsi()
-    df["macd"] = ta.trend.MACD(df["close"]).macd_diff()
-    df["ema20"] = ta.trend.EMAIndicator(df["close"], window=20).ema_indicator()
-    return df
